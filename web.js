cfba08852798308eb7fff7733e38c34fed8b0035
bkit-cpp <84651107+bkit-cpp@users.noreply.github.com>
2022-10-17 11:31:26 +0700

    update

---
+++ web.js
@@ -0,0 +1,9 @@
+var a='1';
+console.log(JSON.parse(a));
+
+var b='"abc"';
+console.log(JSON.parse(b));
+var c='["LeBaKhai","DHSG",3120410232]';
+console.log(JSON.parse(c));
+console.log(JSON.stringify(1));//String
+console.log(JSON.stringify("['Js','ASP']"))
